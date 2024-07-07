1a93a1b3adbc9c54a5e65b96d9373f1f5712b35b
Gilles CRUCHON <gilles.cruchon@gmail.com>
2024-07-07 11:01:16 +0200

    chore(firebaseConfig): isolate config in a file

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { firebaseConfig } from "./firebaseConfig";
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+export const auth = getAuth(app);
+export const user = auth.currentUser;
+export const provider = new GoogleAuthProvider();
